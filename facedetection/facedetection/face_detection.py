151c97d0373007691174fb76403df00a7e74d7b5
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-22 17:28:33 +0300

    face-detecttion: DD adding cap input

--- facedetection/facedetection/face_detection.py
+++ facedetection/facedetection/face_detection.py
@@ -3,14 +3,24 @@ import numpy as np
 import scipy
 import dlib
 import face_recognition
-
+import os
 
 # Face detection using image as input
-# First Step: Loading the images files into numpy arrays
+# First Step: Loading the known images files 
+
+path = 'fd_database'
+employee_images = []
+employee_names = []
+images_list = os.listdir(path)
+
+for image_path in images_list:
+    current_image=  cv2.imread(f'{path}/{image_path}')
+    employee_images.append(current_image)
+    employee_names.append(os.path.splitext(image_path)[0])
 
 
-tom_image = face_recognition.load_image_file('fd_database/Tom-Cruise-1.jpg')
-tom_test = face_recognition.load_image_file('fd_database/leonardo-dicaprio11.jpg')
+# tom_image = face_recognition.load_image_file('fd_database/Tom-Cruise-1.jpg')
+# tom_test = face_recognition.load_image_file('fd_database/leonardo-dicaprio11.jpg')
 
 def face_recog(image):
 
@@ -34,10 +44,28 @@ def face_recog(image):
     
 
 
-# Getting the encodings for the faces
+# Getting the encodings for the known faces
+def encoded_known_list(images):
+    encoded_known_employees = []
+    for image in images:
 
-train_encoded = face_recog(tom_image)[0] 
-test_encoded = face_recog(tom_test)[0]  
+        result = face_recog(image)[0] 
+        encoded_known_employees.append(result)
+    return encoded_known_employees
+
+encodeListKnown = encoded_known_list(employee_images)
+
+
+# Getting the encodings for the unknown faces
+cap = cv2.VideoCapture(0)
+# test_encoded = face_recog(tom_test)[0] 
+# while cap.isOpened():
+#     success, img = cap.read()
+#     imgS = cv2.resize(img,(0,0),None,0.25,0.25) 
+#     if cv2.waitKey(10) & 0xFF == ord('q'):
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
 
 def face_comparison(train_encode,test_encode):
     # Fourth step: Comaring between the the test image and train image measurements
