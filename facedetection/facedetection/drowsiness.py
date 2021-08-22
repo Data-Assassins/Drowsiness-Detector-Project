107111c8e7a1d9fdfc37971c29c05d1c6bfd2d25
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-08-22 18:08:32 +0300

    detection-drow: added drowsiness.py functions

--- facedetection/facedetection/drowsiness.py
+++ facedetection/facedetection/drowsiness.py
@@ -3,10 +3,44 @@ from imutils.video import VideoStream
 from imutils import face_utils
 from threading import Thread
 import numpy as np
-# import playsound
+import pyglet
 import argparse
 import imutils
 import time
 import dlib
 import cv2
 
+def sound_alarm(path):
+    music = pyglet.resource.media('facedetection/alarm.wav')
+    music.play()
+    pyglet.app.run()
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+	help="index of webcam on system")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 48
+
+COUNTER = 0
+ALARAM = False
+
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("facedetection/68_face_landmarks.dat")
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+print( "[INFO] starting video thread ..")
+
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
