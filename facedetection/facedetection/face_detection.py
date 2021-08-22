eda7feb14c22b9a103d7e3a390b744bfb3a40540
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-08-22 13:40:23 +0300

    face-detection: DDupdating steps

--- facedetection/facedetection/face_detection.py
+++ facedetection/facedetection/face_detection.py
@@ -9,34 +9,46 @@ import face_recognition
 # The tarining image and testing from our database(fd_database) directory
 
 # The train image
+# imgElon = face_recognition.load_image_file('fd_database/Tom-Cruise-1.jpg')
 tom_image = face_recognition.load_image_file('fd_database/Tom-Cruise-1.jpg')
 
 # The detection works only on grayscale images
+# imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 tom_image = cv2.cvtColor(tom_image, cv2.COLOR_BGR2GRAY)
 
 # The test image
 tom_test = face_recognition.load_image_file('fd_database/tom_test.jpeg')
 tom_test = cv2.cvtColor(tom_test, cv2.COLOR_BGR2GRAY)
 
-# The Second Step: Get the face location fpr each face in each image.
+# # The Second Step: Get the face location fpr each face in each image.
 
-face_loc = face_recognition.face_locations(tom_image)[0] 
+
+faceLoc = face_recognition.face_locations(tom_image)[0] 
+# print(faceLoc)
+point_one = (faceLoc[3],faceLoc[0])
+point_two = (faceLoc[1],faceLoc[2])
+
 # index zero because we can have multiple faces in one image but we want only the face location for the first one
 # (top,right,bottom,left)
-face_loc_test= face_recognition.face_locations(tom_test)[0]
 
 
 # The Third step: Get the face encodings,for each face in each image file . 
 
-# Trian image
 encod_tom = face_recognition.face_encodings(tom_image)[0]
-cv2.rectangle(tom_image(face_loc[3],face_loc[0]),(face_loc[1],face_loc[2]),(255,0,255),2)
+cv2.rectangle(tom_image,point_one,point_two,(255,0,255),2)
 
 # Test image
+face_loc_test= face_recognition.face_locations(tom_test)[0]
 encod_tom_test = face_recognition.face_encodings(tom_test)[0]
 cv2.rectangle(tom_test,(face_loc_test[3],face_loc_test[0]),(face_loc_test[1],face_loc_test[2]),(255,0,255),2)
 
 
+# Fourth step : Compare between the two images face measurements
 cv2.imshow('Tom Train',tom_image)
 cv2.imshow('Tom Test',tom_test)
 cv2.waitKey(0)
+
+
+
+ 
+
